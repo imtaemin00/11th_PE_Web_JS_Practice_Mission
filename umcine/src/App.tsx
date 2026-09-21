a0daffc76f8d2a47cc60989e861e163d79b20b7b
한태민 <hantanmin90@gmail.com>
2026-09-21 15:04:44 +0900

    practice: 2주차 미니 실습 - 영화 목록 화면을 컴포넌트로 나누기 제출

--- umcine/src/App.tsx
+++ umcine/src/App.tsx
@@ -1,13 +1,34 @@
-export default function App() {
-  const movieTitle = "극장판 짱구는 못말려: 습격!! 외계인 덩덩이";
-  const genre = "애니메이션";
-  const releaseDate = "2017.07.20";
-
+function Header() {
   return (
-    <article className="movie-card">
-      <h1>{movieTitle}</h1>
-      <p>장르: {genre}</p>
-      <p>개봉일: {releaseDate}</p>
+    <header>
+      <h1>UMCine</h1>
+    </header>
+  );
+}
+
+function MovieCard() {
+  return (
+    <article>
+      <h2>극장판 짱구는 못말려</h2>
+      <p>2017.07.20</p>
     </article>
+  );
+}
+
+function MovieList() {
+  return (
+    <section>
+      <MovieCard />
+      <MovieCard />
+    </section>
+  );
+}
+
+export default function App() {
+  return (
+    <main>
+      <Header />
+      <MovieList />
+    </main>
   );
 }
