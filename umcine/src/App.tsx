85c644be4fd300ffa50512d25843ffb95e496b50
한태민 <hantanmin90@gmail.com>
2026-09-21 15:01:40 +0900

    practice: 2주차 미니 실습 - 영화 정보 카드 작성하기 제출

--- umcine/src/App.tsx
+++ umcine/src/App.tsx
@@ -1,11 +1,13 @@
-import './App.css'
+export default function App() {
+  const movieTitle = "극장판 짱구는 못말려: 습격!! 외계인 덩덩이";
+  const genre = "애니메이션";
+  const releaseDate = "2017.07.20";
 
-function App() {
   return (
-    <>
-      <h1>제로의 React 학습</h1>
-    </>
-  )
+    <article className="movie-card">
+      <h1>{movieTitle}</h1>
+      <p>장르: {genre}</p>
+      <p>개봉일: {releaseDate}</p>
+    </article>
+  );
 }
-
-export default App
